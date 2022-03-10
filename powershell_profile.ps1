cc4dc84b92d1b06f9f9d9a7825d30b8a8526edde
Daniel Radosa <danysko5@gmail.com>
2022-03-10 13:46:43 +0100

    Create powershell_profile.ps1

---
+++ powershell_profile.ps1
@@ -0,0 +1,14 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt tiwahu
+Import-Module Terminal-Icons
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Alias
+Set-Alias suck scoop
+Set-Alias some install
+Set-Alias cc cls
+Set-Alias cc clear
+Set-Alias ll ls
